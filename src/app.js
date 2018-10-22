acb8c5a99cecff6853915fbeae609d7be224dd1d
Jean Sotil <jean.sotil@gmail.com>
2018-10-22 11:11:44 -0500

    setting async components.

--- src/app.js
+++ src/app.js
@@ -4,17 +4,18 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 // async component
 import Async from './containers/async-component';
 
-// components
-import Header from './components/header';
-import Footer from './components/footer';
-import PreLoader from './components/commons/preloader';
-
 // styles
 import styles from './resources/styles/index.scss';
 
+// components
+const Header = Async(() => import('./components/header').then(module => module.default), {});
+const Footer = Async(() => import('./components/footer').then(module => module.default), {});
+const PreLoader = Async(() => import('./components/commons/preloader').then(module => module.default), {});
+
+
 // containers
-import HomePage from './containers/home';
-import SearchPage from './containers/search';
+const HomePage = Async(() => import('./containers/home').then(module => module.default), { name: 'Home' });
+const SearchPage = Async(() => import('./containers/search').then(module => module.default), { name: 'Home' });
 
 // main
 class App extends Component {
--- src/store/selectors/search/index.js
+++
@@ -1,6 +0,0 @@
-export const itemsSelector = state => (state.items);
-
-export const selectedItemSelector = state => (state.selectedItem);
-
-// default
-export const defaultSelector = null;
